ef85fd7181eb68b6d71df9868eca33e007244eb2
jlo <jlo@6e660d33-609c-4e8b-a89d-cb94e1c90b59>
2014-12-12 23:37:41 +0000

    Disable map on the search page for smaller screens. Increase the size of the "Ask Agent"

--- include/header.php
+++ include/header.php
@@ -17,48 +17,49 @@ and open the template in the editor.
 //session_start();
 include ('../pathMaker.php');
 ?>
- <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">    
+<style>
+    @media (max-width: 1050px) {
+
+    }
+</style>
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">    
         <div class="container-fluid">
-                <div class="navbar-header" style=" height: 60px;">
-                    <a class="navbar-brand" href="http://www.sfsuswe.com/~<?php echo $pathArray[2] ?>/pages/home.php"> <img src="http://www.sfsuswe.com/~<?php echo $pathArray[2] ?>/images/Logo_surreal.png" class="image-responsive" style="max-height:65px; margin-top: -15px;">
-                    </a>
-                         <button type="button" class="navbar-toggle collapsed"
-                            data-toggle="collapse" data-target="#navbar-collapse">
-                                    <span class="sr-only">Toggle navigation</span>
-                                    <span class="icon-bar"></span>
-                                    <span class="icon-bar"></span>
-                                    <span class="icon-bar"></span>
-                        </button>
-                </div>
-        <!--right part of the navbar-->
-                <div class="collapse navbar-collapse" id="navbar-collapse">
-                    
-                    <!--left part of the navbar-->           
-                    <ul class="nav navbar-nav navbar-left" style="font-size: 1.2em">
-                        <li><a href="http://www.sfsuswe.com/~<?php echo $pathArray[2] ?>/pages/home.php">Home</a></li>
-                        <li><a href="http://www.sfsuswe.com/~<?php echo $pathArray[2] ?>/pages/search.php">Buy</a></li>
-                        <li><a href="#myModal_sell" data-toggle="modal">Sell</a></li>
-                        <li><a href="#">Agents</a></li>
-                        <li><a href="#">Advice</a></li>
-                        <li><a href="#ContactUs" data-toggle="modal">Contact Us</a></li>
-                        <li><a href="#AboutUs" data-toggle="modal">About</a></li> 
-                        <li><a href="#">Local</a></li>
-                    </ul>
-                    
-                    <ul class="nav navbar-nav navbar-right" style="font-size: 1.2em">
-                        <?php 
-                            if(isset($_SESSION['fname'])) {
-                                echo "<li><a href=\"login/".strtolower($_SESSION['role'])."/".strtolower($_SESSION['role'])."Dashboard.php\" ><span class=\"glyphicon glyphicon-user\"></span>&nbsp;Hello&nbsp;".$_SESSION['fname']."</a></li>";
-                                echo "<li><a href=\"http://www.sfsuswe.com/~". $pathArray[2] ."/pages/login/logout.php\"><span class=\"glyphicon glyphicon-log-out\"></span>&nbsp;Logout</a></li>";
-                            } 
-                            else {
-                               echo "<li><a href=\"#myModal_SU\" data-toggle=\"modal\"><span class=\"glyphicon glyphicon-edit\"></span>&nbsp;Register</a></li>";
-                               echo "<li><a href=\"#myModal_SI\" data-toggle=\"modal\"><span class=\"glyphicon glyphicon-log-in\"></span>&nbsp;Login</a></li> ";
-                            }   
-                        ?>
-                      </ul>
-         
-                </div>
+            <div class="navbar-header" style=" height: 60px;">
+                <a class="navbar-brand" href="http://www.sfsuswe.com/~<?php echo $pathArray[2] ?>/pages/home.php"> <img src="http://www.sfsuswe.com/~<?php echo $pathArray[2] ?>/images/Logo_surreal.png" class="image-responsive" style="max-height:65px; margin-top: -15px;">
+                </a>
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <!--right part of the navbar-->
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+                <!--left part of the navbar-->           
+                <ul class="nav navbar-nav" style="font-size: 1.2em">
+                    <li><a href="http://www.sfsuswe.com/~<?php echo $pathArray[2] ?>/pages/home.php">Home</a></li>
+                    <li><a href="http://www.sfsuswe.com/~<?php echo $pathArray[2] ?>/pages/search.php">Buy</a></li>
+                    <li><a href="#myModal_sell" data-toggle="modal">Sell</a></li>
+                    <li><a href="#">Agents</a></li>
+                    <li><a href="#">Advice</a></li>
+                    <li><a href="#ContactUs" data-toggle="modal">Contact Us</a></li>
+                    <li><a href="#AboutUs" data-toggle="modal">About</a></li> 
+                    <li><a href="#">Local</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right" style="font-size: 1.2em">
+                    <?php 
+                        if(isset($_SESSION['fname'])) {
+                            echo "<li><a href=\"login/".strtolower($_SESSION['role'])."/".strtolower($_SESSION['role'])."Dashboard.php\" ><span class=\"glyphicon glyphicon-user\"></span>&nbsp;Hello&nbsp;".$_SESSION['fname']."</a></li>";
+                            echo "<li><a href=\"http://www.sfsuswe.com/~". $pathArray[2] ."/pages/login/logout.php\"><span class=\"glyphicon glyphicon-log-out\"></span>&nbsp;Logout</a></li>";
+                        } 
+                        else {
+                            echo "<li><a href=\"#myModal_SU\" data-toggle=\"modal\"><span class=\"glyphicon glyphicon-edit\"></span>&nbsp;Register</a></li>";
+                            echo "<li><a href=\"#myModal_SI\" data-toggle=\"modal\"><span class=\"glyphicon glyphicon-log-in\"></span>&nbsp;Login</a></li> ";
+                        }   
+                    ?>
+                </ul>
+            </div>
         </div>
     </nav>
 
